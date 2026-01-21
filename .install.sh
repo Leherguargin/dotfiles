d39fcb368ec389d7e1f6c3af84c5efaaf5e1a30f
Maciej Leszczynski <maciejleszczynski1997@gmail.com>
2026-01-22 00:57:05 +0100

    update install script

--- .install.sh
+++ .install.sh
@@ -8,27 +8,32 @@ sudo apt install man info curl wget tar gzip unzip zip git stow nano tree
 #sudo apt install fzf # what is that? 
 #sudo apt install tee # what is that?
 
-# zsh
-sudo apt install -y zsh # install zsh
+if [[ ! $(which zsh) ]]; then
+	sudo apt install -y zsh # install zsh
+fi
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # install oh-my-zsh
 
-# tmux
 #TODO sudo apt install -y byobu # for now I dont use byobu
 if [[ ! $(which tmux) ]]; then
 	sudo apt install -y tmux
 fi
 
+if [[ ! $(which nvim) ]]; then
+	sudo apt install -y neovim
+fi
+
 # net-tools
 #TODO
 
-# neovim
-sudo apt install -y neovim
 
 # c/cpp development tools
 # TODO sudo apt install binutils clang # change test and dokoncz
 
 # sdkman
-curl -s "https://get.sdkman.io" | bash
+if [[ $(which sdk) ]]; then
+	curl -s "https://get.sdkman.io" | bash
+fi
 
 # ts/js
 #TODO
